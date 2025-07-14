f1a59cc0d27faa3ea75ca260c0ac79dd86a790f4
NeoRamashapa <92487322+NeoRamashapa@users.noreply.github.com>
2025-07-14 18:33:57 +0200

    Initial Project setup with ApplicationDbConect and db Connection

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
+using InternalResourceBookingSystem.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -15,7 +21,6 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
